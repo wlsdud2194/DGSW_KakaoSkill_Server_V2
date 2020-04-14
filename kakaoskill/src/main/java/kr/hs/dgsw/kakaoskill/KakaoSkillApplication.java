aaf14631518c82a80248d26668703d0e257fa6ef
wlsdud2194 <38432821+wlsdud2194@users.noreply.github.com>
2020-04-14 11:55:45 +0900

    init project

---
+++ kakaoskill/src/main/java/kr/hs/dgsw/kakaoskill/KakaoSkillApplication.java
@@ -0,0 +1,12 @@
+package kr.hs.dgsw.kakaoskill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaoSkillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakaoSkillApplication.class, args);
+	}
+}
---
+++ kakaoskill/src/test/java/kr/hs/dgsw/kakaoskill/KakaoSkillApplication.java
@@ -0,0 +1,13 @@
+package kr.hs.dgsw.kakaoskill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaoSkillApplication {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
